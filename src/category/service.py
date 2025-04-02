6f2cdc2e2afc60651f945a74e2bf82cf62eedb83
William da Silva Dias <wsil7@hotmail.com>
2025-04-02 16:28:57 -0300

    feat: Create category service

---
+++ src/category/service.py
@@ -0,0 +1,35 @@
+from odmantic import ObjectId
+from odmantic.session import AIOSession
+
+from src.category.models import Category
+from src.category.schema import CategoryCreate, CategoryUpdate
+
+
+async def get_categories(db: AIOSession) -> list[Category]:
+    return await db.find(Category)
+
+
+async def get_category_by_id(db: AIOSession, id: ObjectId) -> Category | None:
+    return await db.find_one(Category, Category.id == id)
+
+
+async def create_category(db: AIOSession, schema: CategoryCreate) -> Category:
+    return await db.save(Category(**schema.model_dump(exclude_unset=True)))
+
+
+async def update_category(
+    db: AIOSession, schema_update: CategoryUpdate, id: ObjectId
+) -> Category | None:
+    actual_model = await db.find_one(Category, Category.id == id)
+    if not actual_model:
+        raise Exception('category not found')
+    actual_model.model_update(schema_update)
+    return await db.save(actual_model)
+
+
+async def delete_category(db: AIOSession, id: ObjectId) -> Category | None:
+    actual_model = await db.find_one(Category, Category.id == id)
+    if not actual_model:
+        raise Exception('category not found')
+    await db.delete(actual_model)
+    return actual_model
