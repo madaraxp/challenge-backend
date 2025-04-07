f72474d0f7cb817d792dfc1acde2c96413611235
William da Silva Dias <wsil7@hotmail.com>
2025-04-07 17:04:21 -0300

    fix: replace AsyncMock with mongomock for ODMantic engine tests

--- tests/category/test_service.py
+++ tests/category/test_service.py
@@ -1,137 +1,117 @@
+from functools import partial
 from unittest.mock import AsyncMock
 
 import pytest
-from odmantic import ObjectId
-from odmantic.session import AIOSession
+import pytest_asyncio
+from mongomock_motor import AsyncMongoMockClient
+from odmantic import AIOEngine, ObjectId
 
 import src.category.service as category_service
 from src.category.models import Category
 from src.category.schema import CategoryCreate, CategoryUpdate
 
 
-@pytest.mark.asyncio
-async def test_get_all_categories():
-    mock_session = AsyncMock(spec=AIOSession)
+@pytest_asyncio.fixture
+async def test_db():
+    client = AsyncMongoMockClient()
+    engine = AIOEngine(client=client, database='test_db')
+
+    mock_session = AsyncMock(spec=engine)
+    mock_session.save = AsyncMock(side_effect=partial(engine._save, session=None))
+    mock_session.find = AsyncMock(side_effect=partial(engine.find))
+    mock_session.find_one = AsyncMock(side_effect=partial(engine.find_one))
+    mock_session.delete = AsyncMock(side_effect=partial(engine.delete))
+
+    return mock_session
+
+
+@pytest_asyncio.fixture
+async def insert_category(test_db):
     category = Category(
         id=ObjectId(),
         title='Test Category',
         description='Test Description',
         owner_id=ObjectId(),
     )
-    mock_session.find = AsyncMock(return_value=[category])
-    response = await category_service.get_categories(mock_session)
-
-    assert response == [category]
-    mock_session.find.assert_called_once_with(Category)
+    await test_db.save(category)
+    return category
 
 
 @pytest.mark.asyncio
-async def test_get_category():
-    mock_session = AsyncMock(spec=AIOSession)
-    category = Category(
-        id=ObjectId(),
-        title='Test Category',
-        description='Test Description',
-        owner_id=ObjectId(),
-    )
-    mock_session.find_one = AsyncMock(return_value=category)
-    response = await category_service.get_category_by_id(mock_session, category.id)
+async def test_get_all_categories(test_db, insert_category):
+    response = await category_service.get_categories(test_db)
 
-    assert response == category
-    mock_session.find_one.assert_called_once_with(Category, Category.id == category.id)
+    assert [cat async for cat in response] == [insert_category]
 
 
 @pytest.mark.asyncio
-async def test_create_category():
-    mock_session = AsyncMock(spec=AIOSession)
+async def test_get_category(test_db, insert_category):
+    response = await category_service.get_category_by_id(test_db, insert_category.id)
+
+    assert response == insert_category
+
+
+@pytest.mark.asyncio
+async def test_create_category(test_db):
     category = CategoryCreate(
-        title='Test Category',
-        description='Test Description',
+        title='Test Create',
+        description='Test Create',
         owner_id=ObjectId(),
     )
-    mock_session.save = AsyncMock(
-        return_value=Category(**category.model_dump(), id=ObjectId())
-    )
-    response = await category_service.create_category(mock_session, category)
+
+    response = await category_service.create_category(test_db, category)
 
     assert response.title == category.title
     assert response.description == category.description
     assert response.owner_id == category.owner_id
-    mock_session.save.assert_called_once()
+    test_db.save.assert_called_once()
 
 
 @pytest.mark.asyncio
-async def test_get_update_category():
-    mock_session = AsyncMock(spec=AIOSession)
-    category = Category(
-        id=ObjectId(),
-        title='Test Category',
-        description='Test Description',
-        owner_id=ObjectId(),
-    )
+async def test_get_update_category(test_db, insert_category):
     category_update = CategoryUpdate(
         title='Test Category Update', description='Test Category Description'
     )
 
-    mock_session.find_one = AsyncMock(return_value=category)
-    mock_session.save = AsyncMock()
-    await category_service.update_category(mock_session, category_update, category.id)
+    await category_service.update_category(test_db, category_update, insert_category.id)
 
-    mock_session.find_one.assert_called_once_with(Category, Category.id == category.id)
-
-    category.model_update(category_update)
-    mock_session.save.assert_called_once_with(category)
+    result = await test_db.find_one(Category, Category.id == insert_category.id)
+    assert result.title == category_update.title
+    assert result.description == category_update.description
 
 
 @pytest.mark.asyncio
-async def test_get_update_category_exception():
-    mock_session = AsyncMock(spec=AIOSession)
-    category = Category(
-        id=ObjectId(),
-        title='Test Category',
-        description='Test Description',
-        owner_id=ObjectId(),
-    )
+async def test_get_update_category_exception(test_db):
     category_update = CategoryUpdate(
         title='Test Category Update', description='Test Category Description'
     )
-
-    mock_session.find_one = AsyncMock(return_value=None)
+    category_id = ObjectId()
+    test_db.find_one = AsyncMock(return_value=None)
 
     with pytest.raises(Exception, match='category not found'):
-        await category_service.update_category(
-            mock_session, category_update, category.id
-        )
+        await category_service.update_category(test_db, category_update, category_id)
 
-    mock_session.find_one.assert_called_once_with(Category, Category.id == category.id)
-    mock_session.save.assert_not_called()
+    test_db.find_one.assert_called_once_with(Category, Category.id == category_id)
+    test_db.save.assert_not_called()
 
 
 @pytest.mark.asyncio
-async def test_get_delete_category():
-    mock_session = AsyncMock(spec=AIOSession)
-    category = Category(
-        id=ObjectId(),
-        title='Test Category',
-        description='Test Description',
-        owner_id=ObjectId(),
+async def test_get_delete_category(test_db, insert_category):
+    response = await category_service.delete_category(test_db, insert_category.id)
+
+    assert response == insert_category
+    test_db.find_one.assert_called_once_with(
+        Category, Category.id == insert_category.id
     )
-    mock_session.find_one = AsyncMock(return_value=category)
-    response = await category_service.delete_category(mock_session, category.id)
-
-    assert response == category
-    mock_session.find_one.assert_called_once_with(Category, Category.id == category.id)
-    mock_session.delete.assert_called_once_with(category)
+    test_db.delete.assert_called_once_with(insert_category)
 
 
 @pytest.mark.asyncio
-async def test_get_delete_category_exception():
-    mock_session = AsyncMock(spec=AIOSession)
-    id = ObjectId()
-    mock_session.find_one = AsyncMock(return_value=None)
+async def test_get_delete_category_exception(test_db):
+    category_id = ObjectId()
 
     with pytest.raises(Exception, match='category not found'):
-        await category_service.delete_category(mock_session, id)
+        await category_service.delete_category(test_db, category_id)
 
-    mock_session.find_one.assert_called_once_with(Category, Category.id == id)
-    mock_session.delete.assert_not_called()
+    test_db.find_one.assert_called_once_with(Category, Category.id == category_id)
+    test_db.delete.assert_not_called()
